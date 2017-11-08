d5a37eb14a4df786dac11025b72875cf1f0ebf50
aphe-lion <tom.0505@hotmail.co.uk>
2017-11-08 23:14:09 +0000

    pep8 and tidying

--- 4gol.py
+++ 4gol.py
@@ -1,38 +1,58 @@
-import pygame
+from __future__ import print_function
+import sys
 from time import sleep
 from random import randint, seed, choice
 from argparse import ArgumentParser
+import pygame
 
-# Board initialising, blank or random
+# Account for lack of xrange in py3
+if sys.version_info[0] == 3:
+    xrange = range
+
+
 def gen_board(size, random=False):
-    if random:
-        return [[choice([0, 0, 0, 1, 1]) for y in xrange(size)] for x in xrange(size)]  
-    
-    return [[0 for y in xrange(size)] for x in xrange(size)]
-      
-# Determines cell state in a new tick
-def next_tick(cell, neighbors): 
-    if cell and neighbors in [2, 3]:
-            return 1
-    elif not cell and neighbors == 3:
-            return 1
-    return 0
+    """Returns a blank or random board"""
 
-# Counts a cell's neighbors
-def count_neighbors(x, y, board, size): 
-    return int(                                 
-        board[x               ][mod(y - 1, size)] +  
-        board[mod(x + 1, size)][mod(y - 1, size)] +  
-        board[mod(x + 1, size)][y             ] +  
-        board[mod(x + 1, size)][mod(y + 1, size)] +  
-        board[x               ][mod(y + 1, size)] +  
-        board[mod(x - 1, size)][mod(y + 1, size)] +  
-        board[mod(x - 1, size)][y             ] +  
+    if random:
+        return [[choice([False, False, False, True, True]) 
+            for y in xrange(size)] for x in xrange(size)]  
+    
+    return [[False for y in xrange(size)] for x in xrange(size)]
+
+
+def next_tick(cell_alive, number_of_neighbors): 
+    """Returns a cell's state in the next turn
+    
+    Three conditions for the next state are layed out:
+    1. A cell is alive and has 2/3 neighbors, it stays alive
+    2. A cell is dead and has 3 neighbors, it is born
+    3. Neither of the above, it dies
+    """
+
+    if cell_alive and number_of_neighbors in [2, 3]:
+            return True
+    elif not cell_alive and number_of_neighbors == 3:
+            return True
+    return False
+
+
+def count_neighbors(x, y, board): 
+    """Returns the number of neighbors a cell has"""
+    size = len(board)
+    return (                                 
+        board[x               ][mod(y - 1, size)] +
+        board[mod(x + 1, size)][mod(y - 1, size)] +
+        board[mod(x + 1, size)][y               ] +
+        board[mod(x + 1, size)][mod(y + 1, size)] +
+        board[x               ][mod(y + 1, size)] +
+        board[mod(x - 1, size)][mod(y + 1, size)] +
+        board[mod(x - 1, size)][y               ] +
         board[mod(x - 1, size)][mod(y - 1, size)]   
     )
 
-# Psuedo-modulo function, used to make a torus
+
 def mod(n, size): 
+    """Returns the position of a cell based on modulo arithmetic"""
     size -= 1
     if n < 0:
         return size
@@ -40,12 +60,12 @@ def mod(n, size):
         return 0
     return n
 
-# This is where the magic happens
-def main():
-    # Print controls
-    print "Controls: R - restart, Esc - quit"
 
-    # Argsparse setup
+def main():
+    """Main loop, defines game logic"""
+    print("Controls: R - restart, Esc - quit")
+
+    # Argument parser setup
     parser = ArgumentParser()
     parser.add_argument("-n", type = int, default = 64, 
                         help = "Number of cells", metavar = "")
@@ -53,7 +73,7 @@ def main():
                         help = "Size of cells", metavar = "")
     parser.add_argument("-d", action = "store_true", 
                         help = "Enable debug mode")
-    parser.add_argument("-t", type = float, default = 0.03, 
+    parser.add_argument("-t", type = float, default = 0.00, 
                         help = "Sets delay between ticks", 
                         metavar = "")
     parser.add_argument("-c", action = "store_true", 
@@ -73,18 +93,19 @@ def main():
     age_cap = args.k
     step_cap = args.r
 
-    board = gen_board(cell_num, True)
-    neighbors = gen_board(cell_num)
-    cell_age = gen_board(cell_num)
+    board = gen_board(cell_num, random=True)
+    neighbors = gen_board(cell_num, random=False)
+    cell_age = gen_board(cell_num, random=False)
     step_counter = 0
 
     # Pygame setup
     pygame.init()
-    display_size = ((cell_size + 1) * cell_num) - 1
-    display_size = (display_size, display_size)
-    display = pygame.display.set_mode(display_size)
-
+    # Calculate and set displays size by generating a 2-tuple
+    display = pygame.display.set_mode(
+        (((cell_size + 1) * cell_num) - 1,) * 2)
     pygame.display.set_caption("Conway's Game of Life")
+
+    # Colours
     c_black = (0, 0, 0)
     c_white = (255, 255, 255)
     c_red = (255, 0, 0)
@@ -96,13 +117,12 @@ def main():
         pygame.event.pump()
         keys_pressed = pygame.key.get_pressed()
 
-        # Act on pressed keys
         if keys_pressed[pygame.K_r] or (step_cap and step_counter > step_cap):
-            board = gen_board(cell_num, True)
+            board = gen_board(cell_num, random=True)
             step_counter = 0
         if keys_pressed[pygame.K_ESCAPE]:
             pygame.quit()
-            exit()
+            sys.exit(0)
 
         # Clear display
         display.fill(c_black)
@@ -114,39 +134,41 @@ def main():
                     # Generate the square to be drawn
                     cell = pygame.Rect((cell_size + 1) * x, 
                                 (cell_size + 1) * y, cell_size, cell_size)
+
                     # Draw the cell
                     if colour_enabled:
                         if cell_age[x][y] == 0:
                             pygame.draw.rect(display, c_blue, cell)
-                        elif cell_age[x][y] in [1, 2, 3, 4, 5, 6]:
+                        elif cell_age[x][y] in [1, 2, 3, 4, 5]:
                             pygame.draw.rect(display, c_white, cell)
+                        elif cell_age[x][y] in [6, 7, 8, 9, 10, 11]:
+                            pygame.draw.rect(display, c_red, cell)
                         else:
                             pygame.draw.rect(display, c_green, cell)
                     else:
                         pygame.draw.rect(display, c_white, cell)
-                    # Increase cell age
+
                     cell_age[x][y] += 1
                 else:
                     cell_age[x][y] = 0         
 
-        # Update display
         pygame.display.update()
  
         # Count Neighbors
         for y in xrange(cell_num):
             for x in xrange(cell_num):
-                neighbors[x][y] = count_neighbors(x, y, board, cell_num)
+                neighbors[x][y] = count_neighbors(x, y, board)
 
         # Update board
         for y in xrange(cell_num):
             for x in xrange(cell_num):
                 board[x][y] = next_tick(board[x][y], neighbors[x][y])
+
                 if age_cap and cell_age[x][y] > age_cap:
-                    board[x][y] = 0
+                    board[x][y] = False
 
         step_counter += 1
 
-        # Delay if in debug mode
         if not debug_mode:
             sleep(time_delay)
         else:
@@ -154,3 +176,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    sys.exit(0)
